d35e395c216aa9474f5b0f21c4ad59845b2a2c3f
王春成 <chuncheng.wang@hand-china.com>
2019-01-28 17:53:05 +0800

    edit script

--- env.sh
+++ env.sh
@@ -5,18 +5,19 @@ dep="node_modules"
 ## npm init
 if [ ! -f $init ] ; then
     npm init
+    # babel
+    npm install --save-dev babel-loader @babel/core @babel/cli @babel/preset-env @babel/preset-react
+    # css/style loader
+    npm install --save-dev css-loader style-loader
+    # react
+    npm install --save-dev react react-dom
 else echo "$init 已存在，跳过初始化"
 fi
 
 
 ## npm dependencies
 if [ ! -d $dep ] ;then
-# babel
-npm install --save-dev babel-loader @babel/core @babel/cli @babel/preset-env @babel/preset-react
-# css/style loader
-npm install --save-dev css-loader style-loader
-# react
-npm install --save-dev react react-dom
+    npm install
 else
     echo "$dep 已存在，跳过依赖解析"
     exit 0
